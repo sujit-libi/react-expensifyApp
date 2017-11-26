1fcd08de52c1b4e1c08ed95b14876ac03cb211df
Sujit Kumar Libi <sujit_libi@hotmail.com>
2017-11-26 17:39:24 +0545

    setup dev dependencies and dist folder with shapshot testing enzyme

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
     return {
         entry:'./src/app.js',
         output: {
-            path: path.join(__dirname,'public'),
+            path: path.join(__dirname,'public','dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -42,6 +42,7 @@ module.exports = (env) => {
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
+            publicPath:'/dist/',
             contentBase: path.join(__dirname,'public'),
             historyApiFallback: true
         }
